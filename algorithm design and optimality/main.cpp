7ab0a4535c2d76f5be3bba638fd701586a57c82c
lilelr <lilelr@163.com>
2016-09-28 14:35:44 +0800

    Initial Commit

---
+++ algorithm design and optimality/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  algorithm design and optimality
+//
+//  Created by YuXiao on 9/28/16.
+//  Copyright © 2016 YuXiao. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
